ce1c20dbc61c324c83a86f3c4558963b6fbcc009
JUAN DE DIOS DEL ANGEL ARRIAGA <120742417+20191163@users.noreply.github.com>
2023-11-25 14:37:29 -0600

    Mensaje descriptivo de los cambios

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,16 @@
-const cacheName = 'mi-app-cache';
+const cacheName = 'Almacen';
 const filesToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
+  '../index.html',
+  './public/manifest.json',
   '../src/index.js',
   '../src/App.js',
   '../src/componentes/views/loading/loading.js',
   '../src/componentes/views/loading/styles.css',
+  '../src/componentes/views/footer/footer.js',
+  '../offline.html',
+  '../src/componentes/img/juandedios.jpg',
+  '',
 ];
 
 self.addEventListener('install', (e) => {
@@ -20,7 +24,35 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+      return response || fetch(e.request).then((fetchResponse) => {
+        if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+          return fetchResponse;
+        }
+
+        const responseToCache = fetchResponse.clone();
+
+        caches.open(cacheName).then((cache) => {
+          cache.put(e.request, responseToCache);
+        });
+
+        return fetchResponse;
+      }).catch(() => {
+        // Si la solicitud falla y no hay caché disponible, muestra una página de "offline"
+        return caches.match('/offline.html');
+      });
+    })
+  );
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((existingCacheName) => {
+          if (existingCacheName !== cacheName) {
+            return caches.delete(existingCacheName);
+          }
+        })
+      );
     })
   );
 });
